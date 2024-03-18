67ecc7132cedce1948e3ff34f5b7c6e5ac1da66f
Jose Rubio <jadrubio@gmail.com>
2024-03-18 14:54:15 -0400

    movie list and movie info components

---
+++ src/pages/Movie.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import Layout from "./Layout";
+import { Link, useParams } from "react-router-dom";
+
+const Movie = () => {
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const myMovie = {
+      id: 1,
+      title: "The Secret Life of Walter Mitty",
+      release_date: "2013-10-05",
+      runtime: 114,
+      rated: "PG",
+      description:
+        "When both he and a colleague are about to lose their job, Walter takes action by embarking on an adventure more extraordinary than anything he ever imagined.",
+    };
+    setMovie(myMovie);
+  }, [id]);
+
+  const { title, release_date, runtime, rated, description } = movie;
+  return (
+    <Layout title={title}>
+      <p>{description}</p>
+      <small>
+        <em>
+          {release_date}, {runtime} minutes. Rated {rated}
+        </em>
+      </small>
+    </Layout>
+  );
+};
+
+export default Movie;
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,9 +1,56 @@
+import { useEffect, useState } from "react";
 import Layout from "./Layout";
+import { Link } from "react-router-dom";
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    let moviesList = [
+      {
+        id: 1,
+        title: "The Secret Life of Walter Mitty",
+        release_date: "2013-10-05",
+        runtime: 114,
+        rated: "PG",
+        description:
+          "When both he and a colleague are about to lose their job, Walter takes action by embarking on an adventure more extraordinary than anything he ever imagined.",
+      },
+      {
+        id: 2,
+        title: "Empire Records",
+        release_date: "1995-09-22",
+        runtime: 90,
+        rated: "PG-13",
+        description:
+          "A day in the life of the employees of Empire Records. Except this is a day where everything comes to a head for a number of them facing personal crises - can they pull through together? And more importantly, can they keep their record store independent and not swallowed up by corporate greed?",
+      },
+    ];
+    setMovies(moviesList);
+  }, []);
+
   return (
     <Layout title="Movies">
-      <div className="text-left">Movie Info</div>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Movie</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.id}>
+              <td>
+                <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+              </td>
+              <td>{movie.release_date}</td>
+              <td>{movie.rated}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 };
